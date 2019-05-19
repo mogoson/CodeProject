ac5c4ce49f165d2364048f4ddfbdb9d41b8c2da7
Mogoson <mogoson@outlook.com>
2019-05-19 13:44:09 +0800

    Rename.

--- UCommonEditor/Properties/AssemblyInfo.cs
+++ UCommonEditor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MGS.CommonEditor")]
+[assembly: AssemblyTitle("MGS.UCommonEditor")]
 [assembly: AssemblyDescription("Custom editor for unity script")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MGS.CommonEditor")]
+[assembly: AssemblyProduct("MGS.UCommonEditor")]
 [assembly: AssemblyCopyright("Copyright ©  2018-2019 Mogoson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
