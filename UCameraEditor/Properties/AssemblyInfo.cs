7e3a3bf9930e883872e925f3687aa1c34202db61
Mogoson <mogoson@outlook.com>
2019-06-01 15:04:32 +0800

    Remove invalid script.

--- UCameraEditor/Properties/AssemblyInfo.cs
+++ UCameraEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MGS.UCameraEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2017-2019")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2019 Mogoson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
