19b64ec53f3bd440b41e5550276fc7d15db0473f
Mogoson <mogoson@outlook.com>
2019-06-29 01:52:50 +0800

    Add InteractableUI.

---
+++ UCommon/UI/Abstract/InteractableUI.cs
@@ -0,0 +1,25 @@
+﻿/*************************************************************************
+ *  Copyright © 2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  InteractableUI.cs
+ *  Description  :  Base class for interactable UI.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  6/29/2019
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+namespace MGS.UCommon.UI
+{
+    /// <summary>
+    /// Base class for interactable UI.
+    /// </summary>
+    public abstract class InteractableUI : UIElement, IInteractableUI
+    {
+        /// <summary>
+        /// UI is interactable?
+        /// </summary>
+        public abstract bool Interactable { set; get; }
+    }
+}
