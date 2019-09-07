f26c04f8ae769b4a3cc133c4a4892c45bb30448b
Mogoson <mogoson@outlook.com>
2019-09-08 02:55:39 +0800

    Squashed commit of the following:
    commit 03bec900f333c2696548c6cb11b7b8c3692f867c
    Author: Mogoson <mogoson@outlook.com>
    Date:   Sun Sep 8 02:24:58 2019 +0800
    
        Rename WinNetwork to Network and Add Comdlg32 lib.
    
    commit 1e73c0ba96b42b657b9f32798f753fe10e1a2de4
    Author: Mogoson <mogoson@outlook.com>
    Date:   Sun Sep 8 02:23:02 2019 +0800
    
        Remove invalid reference.
    
    commit 08bb20a06d9ffcf03b4b93186348ba06f55047a4
    Author: Mogoson <mogoson@outlook.com>
    Date:   Fri Aug 30 21:53:17 2019 +0800
    
        Add MonoEventTrigger.
    
    commit 3b26fca9391ebae09b40316c7e5aa23dc921006b
    Author: Mogoson <mogoson@outlook.com>
    Date:   Fri Aug 30 01:36:52 2019 +0800
    
        Update log info.
    
    commit 19bd05e6a38d4f49d737c683d1075026495fe6b7
    Author: Mogoson <mogoson@outlook.com>
    Date:   Fri Aug 30 00:34:30 2019 +0800
    
        Update GenericEvent.
    
    commit 96d9b9f190626998c51e6cb2ea6016ede5a70c65
    Author: Mogoson <mogoson@outlook.com>
    Date:   Thu Aug 29 22:29:18 2019 +0800
    
        Add ExplorerUtility.
    
    commit f13b62e41e9fe9c4d500c33d2361c9e17894f79a
    Author: Mogoson <mogoson@outlook.com>
    Date:   Sun Aug 11 21:40:15 2019 +0800
    
        Update MultilingualUtility
    
    commit 3e232a40c2fedd7dfe85e71875593eced3f5f6a2
    Author: Mogoson <mogoson@outlook.com>
    Date:   Sat Aug 10 02:17:52 2019 +0800
    
        Update WinNetworkUtility
    
    commit c7fbf27a2026a87450713072b4399c97cf315846
    Author: Mogoson <mogoson@outlook.com>
    Date:   Fri Aug 9 23:10:35 2019 +0800
    
        Adjust project.
    
    commit 1b919435245ac24ca11ee3f7d1d6870186a7a149
    Author: Mogoson <mogoson@outlook.com>
    Date:   Fri Aug 9 00:26:27 2019 +0800
    
        Add WinLibrary.csproj
    
    commit 748b2f7e4fe4f3b8dc0a167faa5c54afb4f7e431
    Author: Mogoson <mogoson@outlook.com>
    Date:   Thu Aug 8 23:17:47 2019 +0800
    
        Update MultilingualUtility
    
    commit d28031544dfe0b038429c4b5db836617391083e7
    Author: Mogoson <mogoson@outlook.com>
    Date:   Wed Aug 7 22:24:52 2019 +0800
    
        Update common API
    
    commit 176bcd6a59c13c4626ce9bf56865c9055accbfd5
    Author: Mogoson <mogoson@outlook.com>
    Date:   Wed Aug 7 21:25:43 2019 +0800
    
        Modify Refresh API of MonoUI.
    
    # Conflicts:
    #	CodeProject.sln
    #	WinLibrary/WinInet.cs
    #	WinLibrary/WinLibrary.csproj

---
+++ WinCommon/Network/NetworkListener.cs
@@ -0,0 +1,68 @@
+﻿/*************************************************************************
+ *  Copyright © 2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  NetworkListener.cs
+ *  Description  :  Windows network listener.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  8/8/2019
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using MGS.Common.Generic;
+using MGS.UCommon.DesignPattern;
+using UnityEngine;
+
+namespace MGS.WinCommon.Network
+{
+    /// <summary>
+    /// Windows network listener.
+    /// </summary>
+    [AddComponentMenu("MGS/WinCommon/Network/NetworkListener")]
+    public sealed class NetworkListener : SingleMonoBehaviour<NetworkListener>
+    {
+        #region Field and Property
+        /// <summary>
+        /// Current state of windows network.
+        /// </summary>
+        public NetworkConnState CurrentState { private set; get; }
+
+        /// <summary>
+        /// Event on network state changed.
+        /// </summary>
+        public GenericEvent<NetworkConnState> OnStateChanged { get; } = new GenericEvent<NetworkConnState>();
+
+        /// <summary>
+        /// Last state of windows network.
+        /// </summary>
+        private NetworkConnState lastState;
+        #endregion
+
+        #region Private Method
+        /// <summary>
+        /// Awake component.
+        /// </summary>
+        protected override void SingleAwake()
+        {
+            base.SingleAwake();
+
+            CurrentState = NetworkUtility.GetNetworkConnectState();
+            lastState = CurrentState;
+        }
+
+        /// <summary>
+        /// Update listener.
+        /// </summary>
+        private void Update()
+        {
+            CurrentState = NetworkUtility.GetNetworkConnectState();
+            if (lastState != CurrentState)
+            {
+                lastState = CurrentState;
+                OnStateChanged.Invoke(CurrentState);
+            }
+        }
+        #endregion
+    }
+}
---
+++ WinCommon/Network/NetworkUtility.cs
@@ -0,0 +1,44 @@
+﻿/*************************************************************************
+ *  Copyright © 2019 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  WinNetworkUtility.cs
+ *  Description  :  Utility for windows network.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  8/8/2019
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using MGS.WinLibrary;
+
+namespace MGS.WinCommon.Network
+{
+    /// <summary>
+    /// Utility for windows network.
+    /// </summary>
+    public static class NetworkUtility
+    {
+        #region Public Method
+        /// <summary>
+        /// Retrieves the connected state of the local system.
+        /// </summary>
+        /// <returns>The connected state of the local system.</returns>
+        public static NetworkConnState GetNetworkConnectState()
+        {
+            if (Wininet.InternetGetConnectedState(out uint lpdwFlags, 0))
+            {
+                if ((lpdwFlags & InetFlags.INTERNET_CONNECTION_MODEM) != 0)
+                {
+                    return NetworkConnState.ONLINE_MODEM;
+                }
+                else if ((lpdwFlags & InetFlags.INTERNET_CONNECTION_LAN) != 0)
+                {
+                    return NetworkConnState.ONLINE_LAN;
+                }
+            }
+            return NetworkConnState.OFFLINE;
+        }
+        #endregion
+    }
+}
