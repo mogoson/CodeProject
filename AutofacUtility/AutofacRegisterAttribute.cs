4f48e193640a2e984bced101dbc214e1810719a2
Mogoson <mogoson@outlook.com>
2021-04-10 23:59:53 +0800

    Optimize AutofacRegisterAttribute

--- AutofacUtility/AutofacRegisterAttribute.cs
+++ AutofacUtility/AutofacRegisterAttribute.cs
@@ -33,43 +33,5 @@ namespace Autofac
         /// The Type for service.
         /// </summary>
         public Type ServiceType { set; get; }
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public AutofacRegisterAttribute() { }
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="singleton">Is singleton mode?</param>
-        public AutofacRegisterAttribute(bool singleton)
-        {
-            Singleton = singleton;
-        }
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="serviceKey">The key for service.</param>
-        /// <param name="serviceType">The Type for service.</param>
-        public AutofacRegisterAttribute(object serviceKey, Type serviceType)
-        {
-            ServiceKey = serviceKey;
-            ServiceType = serviceType;
-        }
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="singleton">Is singleton mode?</param>
-        /// <param name="serviceKey">The key for service.</param>
-        /// <param name="serviceType">The Type for service.</param>
-        public AutofacRegisterAttribute(bool singleton, object serviceKey, Type serviceType)
-        {
-            Singleton = singleton;
-            ServiceKey = serviceKey;
-            ServiceType = serviceType;
-        }
     }
 }
