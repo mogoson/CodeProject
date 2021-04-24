db279197c2ba6bd14a19dc4c37906e9d643cf69a
Mogoson <mogoson@outlook.com>
2021-04-24 23:31:06 +0800

    Add Unit test project.

---
+++ UnitTest/Serialization/JsonUtilityProTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MGS.UCommon.Serialization.Tests
+{
+    [TestClass()]
+    public class JsonUtilityProTests
+    {
+        [TestMethod()]
+        public void FromJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FromAvatarJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToAvatarJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FromJsonTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToJsonTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToListAvatarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FromListAvatarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
